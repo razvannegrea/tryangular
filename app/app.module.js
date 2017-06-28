8bd1462b1c54342ce43a3046f1f4fc686947d930
Razvan Negrea <razvan.negrea2@systematic.com>
2017-06-28 17:53:32 +0300

    Modifications.

--- app/app.module.js
+++ app/app.module.js
@@ -3,4 +3,4 @@
  */
 'use strict'
 
-angular.module('tryangular', []);
+angular.module('tryangular', ['blogList']);
---
+++ app/blog-list/blog-list.component.js
@@ -0,0 +1,9 @@
+/**
+ * Created by negr on 6/28/2017.
+ */
+'use strict';
+
+angular.module('blogList').controller('BlogListController', function () {
+    console.log("hello from blogList controller");
+})
+    //.component('blogList');
