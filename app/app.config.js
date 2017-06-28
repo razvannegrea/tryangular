8a15281533c6da1d5e014633f5ebb743be4302b5
Razvan Negrea <razvan.negrea2@systematic.com>
2017-06-28 17:42:12 +0300

    Initial commit

---
+++ app/app.config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by negr on 6/28/2017.
+ */
+'use strict';
+angular.module('tryangular').config(function() {} );
---
+++ app/app.module.js
@@ -0,0 +1,6 @@
+/**
+ * Created by negr on 6/28/2017.
+ */
+'use strict'
+
+angular.module('tryangular', []);
