73e3cbd04b987b5b140739a93a7b66900908a86f
Razvan Negrea <razvan.negrea2@systematic.com>
2017-06-29 13:58:51 +0300

    Modifications.

--- app/blog-list/blog-list.component.js
+++ app/blog-list/blog-list.component.js
@@ -3,7 +3,25 @@
  */
 'use strict';
 
-angular.module('blogList').controller('BlogListController', function () {
-    console.log("hello from blogList controller");
-})
-    //.component('blogList');
+angular.module('blogList')
+    .component('blogList', {
+        templateUrl: "/templates/blog-list.html",
+        controller: function($scope) {
+            var blogItems = [
+                {title: "Blog title 1", id: 1, description: "Some description 1"},
+                {title: "Blog title 2", id: 2, description: "Some description 2"},
+                {title: "Blog title 3", id: 3, description: "Some description 3"},
+                {title: "Blog title 4", id: 4, description: "Some description 4"}
+            ];
+
+            $scope.items = blogItems;
+
+            $scope.title = 'Hi, there';
+            $scope.clicks = 0;
+            $scope.someClickTest = function() {
+                console.log('Clicked');
+                $scope.clicks += 1;
+                $scope.title = 'Clicked ' + $scope.clicks;
+            }
+        }
+    });
