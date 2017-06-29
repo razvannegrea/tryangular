293bb6f359667077820d46777cc1256b0d8be41b
Razvan Negrea <razvan.negrea2@systematic.com>
2017-06-29 15:27:33 +0300

    Switched to lite-server.

--- app/app.config.js
+++ app/app.config.js
@@ -7,6 +7,7 @@ angular.module('tryangular')
         $locationProvider.html5Mode({ enabled: true });
         $routeProvider
             .when("/", { template: "<blog-list></blog-list>" })
+            .when("/about", { template: "<h1>About us</h1>" })
             .when("/blog/1", { template: "<h1> Hi, from blog 1 </h1>" })
             .when("/blog/2", { template: "<blog-list></blog-list>" })
             .otherwise({ template: "Not found !" });
