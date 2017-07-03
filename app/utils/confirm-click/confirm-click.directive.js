b95f7b9ad199a92084b383786fd56204758f90b3
Razvan Negrea <razvan.negrea2@systematic.com>
2017-07-03 11:45:19 +0300

    Navigating by means of a directive.

--- app/utils/confirm-click/confirm-click.directive.js
+++ app/utils/confirm-click/confirm-click.directive.js
@@ -2,26 +2,53 @@
  * Created by negr on 6/30/2017.
  */
 'use strict';
-angular.module('confirmClick')
-    .directive('confirmClick', function() {
-        return {
-            scope: {
-                message: "@message",
-                eq: "=eq",
-                post: "=post"
-            },
-            restrict: "E",
-            template: "<a ng-href='/blog/{{ post.id }}'>{{ post.title }} - {{ post.publishDate }}</a>",
-            link: function (scope, element, attr) {
 
-                var msg = scope.message || "Are you sure ?";
-                //console.log(scope.message);
-                //console.log(scope.eq);
-                //console.log(scope.post);
-                //console.log(element);
-                //console.log(attr);
-                //console.log(attr.ngHref);
-                //console.log(attr.href);
-            }
-        }
+angular.module("confirmClick")
+    .directive('confirmClick', function() {
+       return {
+           restrict: "A",
+           link: function (scope, element, attr) {
+               var msg = attr.confirmClick || "Are you sure ?";
+               var clickAction = attr.confirmedClick;
+               element.bind('click', function (event) {
+                  if (window.confirm(msg)) {
+                      scope.$eval(clickAction);
+                  }
+               });
+           }
+       }
     });
+
+
+// angular.module('confirmClick')
+//     .directive('confirmClick', function($rootScope, $location) {
+//         return {
+//             scope: {
+//                 message: "@message",
+//                 eq: "=eq",
+//                 post: "=post"
+//             },
+//             restrict: "E",
+//             template: "<a ng-href='#'>{{ post.title }} - {{ post.publishDate }}</a>",
+//             link: function (scope, element, attr) {
+//
+//                 var msg = scope.message || "Are you sure ?";
+//                 element.bind("click", function(event){
+//                    if (window.confirm(msg)){
+//                        //console.log('/blog/' + scope.post.id);
+//                        $rootScope.$apply(function(){
+//                            $location.path("/blog/" + scope.post.id);
+//                        });
+//
+//                    }
+//                 });
+//                 //console.log(scope.message);
+//                 //console.log(scope.eq);
+//                 //console.log(scope.post);
+//                 //console.log(element);
+//                 //console.log(attr);
+//                 //console.log(attr.ngHref);
+//                 //console.log(attr.href);
+//             }
+//         }
+//     });
