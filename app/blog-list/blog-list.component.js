03fd101133d2c323e48dc319fe66abe9618b7213
Razvan Negrea <razvan.negrea2@systematic.com>
2017-07-05 15:25:24 +0300

    Dynamic styling (item arrangement).

--- app/blog-list/blog-list.component.js
+++ app/blog-list/blog-list.component.js
@@ -7,12 +7,64 @@ angular.module('blogList')
     .component('blogList', {
         templateUrl: "/templates/blog-list.html",
         controller: function(Post, $location, $routeParams, $rootScope, $scope) {
-            $scope.items = Post.query();
             $scope.title = 'Hi, there';
+
             $scope.goToItem = function(post) {
                 $rootScope.$apply(function(){
                     $location.path("/blog/" + post.id)
                 });
             }
+
+            $scope.changeCols = function(numberOfCols) {
+                if (angular.isNumber(numberOfCols)) {
+                    $scope.numCols = numberOfCols;
+                }else {
+                    $scope.numCols = 2;
+                }
+            }
+
+            $scope.loadingQuery = false;
+            $scope.$watch(function() {
+               if ($scope.query) {
+                   $scope.loadingQuery = true;
+                   $scope.cssClass = 'col-sm-12';
+               } else{
+                   if ($scope.loadingQuery)
+                   {
+                       setupCol($scope.items, 2);
+                       $scope.loadingQuery = false;
+                   }
+               }
+            });
+
+            function setupCol(data, numberOfCols) {
+                $scope.items = data;
+                //console.log("# of ITEMS " + data.length);
+                if (angular.isNumber(numberOfCols)) {
+                    $scope.numCols = numberOfCols;
+                }else {
+                    $scope.numCols = 2;
+                }
+                $scope.cssClass = 'col-sm-'+(12/$scope.numCols);
+                $scope.colItems = chunkArrayInGroups($scope.items, $scope.numCols);
+                //console.log("# of COL ITEMS " + $scope.colItems.length);
+            }
+
+            function chunkArrayInGroups(array, unit) {
+                var results = [];
+                var length = Math.ceil(array.length/unit);
+                for (var i = 0; i < length; i++){
+                    results.push(array.slice(i*unit, (i+1) * unit));
+                }
+                return results;
+            }
+
+            Post.query(
+                function(data) {
+                    setupCol(data, 4);
+                },
+                function(errorData) {
+
+                });
         }
     });
